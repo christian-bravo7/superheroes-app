81f315a1d78dc063b074e6de08e6322a7704102d
Christian Bravo <christian151.bravo@gmail.com>
2021-05-08 10:33:20 -0600

    :lipstick: (colors) adding new colors stylesheet for project

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import Header from '@/components/Header/Header.component';
 
+import classes from '@/App.module.scss';
+
 const App = (): JSX.Element => {
   return (
-    <main>
+    <main className={classes.app}>
       <Header />
       Hello world
     </main>
--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Logo from '@/components/Logo/Logo.component';
 
-import classes from '@/components/header/header.module.scss';
+import classes from '@/components/Header/Header.module.scss';
 
 const Header = (): JSX.Element => {
   return (
