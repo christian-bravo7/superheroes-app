2ebace5e38bffd4f606a8c738ba43553d2de7d03
Christian Bravo <christian151.bravo@gmail.com>
2021-05-08 09:05:35 -0600

    :fire: (default project files) remove innecesary files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+
 import App from '@/App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const helloWorldText = screen.getByText(/Hello world/i);
+
+  expect(helloWorldText).toBeInTheDocument();
 });
